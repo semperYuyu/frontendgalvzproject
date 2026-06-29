1f434d0b4594c3b8d044323e8bb40dda80abdbf2
Tafari <tafarigriffith2016@gmail.com>
2026-06-29 13:24:50 -0600

    more verbose error log

--- frontend/src/components/functions/queryFunctions.js
+++ frontend/src/components/functions/queryFunctions.js
@@ -56,6 +56,8 @@ export const submitSignUp = async (
 
   const data = await req.json();
   if (!data.error) {
+    console.log(data);
+    console.log("set data V");
     console.log(data.data.id);
     Cookies.set("username", username);
     Cookies.set("userId", data.data.id);
